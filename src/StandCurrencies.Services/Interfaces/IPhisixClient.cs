11ce2b5304a6bffed2b3568d0d6eefcce205894e
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-05-10 16:17:09 +0300

    Добавлен клиент к стороннему api

---
+++ src/StandCurrencies.Services/Interfaces/IPhisixClient.cs
@@ -0,0 +1,11 @@
+﻿using StandCurrencies.Services.Resources;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StandCurrencies.Services.Interfaces
+{
+    public interface IPhisixClient
+    {
+        Task<IEnumerable<DataModel>> GetLastData();
+    }
+}
---
+++ src/StandCurrencies.Services/Resources/DataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StandCurrencies.Services.Resources
+{
+    public class DataModel
+    {
+        public string name { get; set; }
+
+        public int volume { get; set; }
+
+        public double amount { get; set; }
+    }
+}
