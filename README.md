f4d2fd7191ca2d6107d2f821e1a4ec9851181f74
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-05-11 14:36:21 +0300

    Удалены лишние вьюшки

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Test-RESULTANT
+Тестовое задание для компании "RESULTANT".
+
+Backend: ASP.NET Core 2.0, SignalR
+Frontend: HTML5, CSS3, JQuery
---
+++ src/StandCurrencies/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StandCurrencies.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace StandCurrencies.Controllers
+{
+    [Route("api")]
+    public class HomeController : Controller
+    {
+        private readonly IPhisixClient _phisixClient;
+
+        public HomeController(IPhisixClient phisixClient)
+        {
+            _phisixClient = phisixClient ?? throw new ArgumentNullException(nameof(IPhisixClient));
+        }
+
+        [HttpGet]
+        public async Task<object> GetLastData(int count = 50)
+        {
+            return await _phisixClient.GetLastData();
+        }
+    }
+}
