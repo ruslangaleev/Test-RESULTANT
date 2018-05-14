143c10cea472793eaa2e82ec962800ae93e39323
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-05-14 08:12:02 +0300

    Вывод только 50 элементов

--- src/StandCurrencies/Controllers/HomeController.cs
+++ src/StandCurrencies/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using StandCurrencies.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace StandCurrencies.Controllers
 {
@@ -33,7 +34,9 @@ namespace StandCurrencies.Controllers
             List<DataModel> dataModels = new List<DataModel>();
 
             var result = await _phisixClient.GetData();
-            foreach (var entry in result.stock)
+
+            // TODO: will add pagination?
+            foreach (var entry in result.stock.Take(50))
             {
                 dataModels.Add(new DataModel
                 {
